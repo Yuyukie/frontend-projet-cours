feb98288ade01d7840363e0e3fcf80a5c434d7d3
Yuyukie <mazars.benjamin@hotmail.fr>
2024-10-25 11:47:13 +0200

    gros commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,86 @@
-"use client";
+"use client"; // Indique que ce composant doit être exécuté côté client
 
-import { CookieZone } from "@/components/CookieZone";
-import { useMemo, useState } from "react";
-import { ShopItem } from "@/components/ShopItem"
+import { CookieZone } from "@/components/CookieZone"; // Importation du composant CookieZone pour afficher les cookies
+import { useCallback, useEffect, useState } from "react"; // Importation des hooks useMemo et useState de React
+import { ShopItem } from "@/components/ShopItem"; // Importation du composant ShopItem pour afficher les articles de la boutique
 
+
+// Définition d'une interface TypeScript pour décrire la structure d'un article de la boutique
 export interface ShopItemType {
-  image_url: string,
-  label: string,
-  price: number,
-  cps: number //Cookies per second
+  id: number,
+  image_url: string; // URL de l'image de l'article
+  label: string; // Nom de l'article
+  price: number; // Prix de l'article
+  cps: number; // Cookies par seconde générés par l'article
+  total: number;
 }
 
+const defaultshopItems : ShopItemType[] =[
+  { 
+    id:1,
+    label: "Mamy", // Nom de l'article
+    image_url: "https://img.freepik.com/photos-gratuite/personnage-dessin-anime-3d_23-2151021890.jpg?t=st=1729697464~exp=1729701064~hmac=6f4d2a3e473ec863acb6c43e24f6bc3b9a3658a166b81dcc097425bf17ae3ea1&w=740", // URL de l'image
+    price: 10, // Prix de l'article
+    cps: 1, // Cookies générés par seconde
+    total: 0
+  },
+    {
+    id:2,
+    label: "SuperMamy", // Nom de l'article
+    image_url: "https://img.freepik.com/photos-gratuite/personnage-dessin-anime-3d_23-2151021890.jpg?t=st=1729697464~exp=1729701064~hmac=6f4d2a3e473ec863acb6c43e24f6bc3b9a3658a166b81dcc097425bf17ae3ea1&w=740", // URL de l'image
+    price: 100, // Prix de l'article
+    cps: 100, // Cookies générés par seconde
+    total : 0,
+  },
+];
 
 export default function Home() {
+  // Déclaration de l'état pour le nombre de cookies, initialisé à 0
+  const [cookies, setCookies] = useState(0);
+  const [purchasedItems, setPurchasedItems] = useState(defaultshopItems)
+  const [cookiesPerSecond, setCookiesPersecond] = useState(0)
 
-  const [cookies, setCookies] = useState(0)
+  const handlePurchasedItem = (item : ShopItemType) => {
 
-  const shopItems = useMemo<ShopItemType[]>(() => {
-    return [
-      { label : "Mamy",
-        image_url : "https://img.freepik.com/photos-gratuite/personnage-dessin-anime-3d_23-2151021890.jpg?t=st=1729697464~exp=1729701064~hmac=6f4d2a3e473ec863acb6c43e24f6bc3b9a3658a166b81dcc097425bf17ae3ea1&w=740", 
-        price:10, cps:1},
-      { label : "SuperMamy",
-        image_url : "https://static.wikia.nocookie.net/studio-ghibli/images/d/d8/Zeniba.png/revision/latest?cb=20180917115708",
-        price: 100, cps: 10},
-    ]
-  }, [cookies])
+    setCookies(cookies - item.price)
 
+    let actualItems = [...purchasedItems]
+    const itemIndex = actualItems.findIndex(o => o.id == item.id)
+    actualItems[itemIndex].total++
+
+    setPurchasedItems([... actualItems])
+    setCookiesPersecond(cookiesPerSecond + item.cps)
+  }
+
+
+  useEffect(() => {
+    const interval = setInterval (() => {
+        setCookies(cookies + cookiesPerSecond / 100)
+      }, 10 )
+      return() => {clearInterval(interval)}
+  })
+ 
   return (
-    <div className="h-screen w-screen flex">
-      <div className="left w-1/4 bg-green-500">
-        <CookieZone totalCookies={cookies} onCookieClick={() => { setCookies(cookies + 1) }} />
+    <div className="h-screen w-screen flex"> {/* Conteneur principal avec une hauteur et largeur complètes */}
+      <div className="left w-1/4 bg-green-500 flex flex col item-center"> {/* Section gauche avec une largeur de 25% et fond vert */}
+        <CookieZone 
+          totalCookies={cookies} // Passer le nombre total de cookies au composant CookieZone
+          cps={cookiesPerSecond}
+          onCookieClick={() => { setCookies(cookies + 1); }} // Fonction pour incrémenter les cookies au clic
+        />
       </div>
-      <div className="center flex-1 bg-red-500">
+      <div className="center flex-1 bg-red-500"> {/* Section centrale prenant le reste de l'espace avec un fond rouge */}
+
       </div>
-      <div className="right w-1/4 bg-yellow-500">
-      {shopItems.map(item =>
-        <ShopItem item={item} onClick={() => {setCookies(cookies - item.price)}}/>
-      )}
+      <div className="right w-1/4  flex flex-col gap-3"> {/* Section droite avec une largeur de 25% et fond jaune */}
+        {purchasedItems.map(item => ( // Itération sur chaque article de la boutique
+          <ShopItem 
+            item={item} // Passer l'article actuel au composant ShopItem
+            totalCookies={cookies}
+            key={item.id}
+            onClick={() => { handlePurchasedItem(item) }} // Fonction pour soustraire le prix des cookies lors de l'achat
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/CookieZone.tsx
+++ src/components/CookieZone.tsx
@@ -1,18 +1,27 @@
-import Image from "next/image"
-import CookieImg from '@/assets/cookie1.png'
+import Image from "next/image"; // Importation du composant `Image` de Next.js pour gérer les images de manière optimisée
+import CookieImg from '@/assets/cookie1.png'; // Importation de l'image du cookie depuis le dossier d'actifs locaux
 
+// Déclaration de l'interface TypeScript `Props` pour typer les propriétés que le composant va recevoir
 interface Props {
-    totalCookies: number,
-    cookiesPerSecond?: number
-    onCookieClick: () => void
+    totalCookies: number; // Le nombre total de cookies, de type `number`
+    onCookieClick: () => void; // Fonction déclenchée lors d'un clic sur le cookie
+    cps : number;
 }
 
-export const CookieZone = ({totalCookies, onCookieClick}:Props) => {
+// Définition du composant `CookieZone`, qui accepte les propriétés typées avec l'interface `Props`
+export const CookieZone = ({ totalCookies, onCookieClick, cps = 0 }: Props) => {
 
-    return(
-        <div className="h-full w-full bg-purple-700 flex flex-col items-center justify-center gap-10">
-            <p className="text-3xl font-bold">Total cookies : {totalCookies}</p>
-            <Image onClick={() => onCookieClick()} src={CookieImg} alt="Big Cookie" className="cursor-pointer h-60 w-60 hover:scale-110 ease-in-out duration-300" />
+    return (
+        <div className="h-full w-full bg-purple-700 flex flex-col gap-10">
+            <p className="">{cps} Cookies par seconde</p>
+            <p className="text-3xl font-bold">Total cookies : {totalCookies.toFixed(0)}</p>
+            <Image 
+                onClick={() => onCookieClick()} 
+                src={CookieImg} 
+                alt="Big Cookie" 
+                className="cursor-pointer h-60 w-60 hover:scale-110 ease-in-out duration-300" 
+                
+            />
         </div>
-    )
-}
+    );
+};
